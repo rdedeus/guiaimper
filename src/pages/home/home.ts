e07cc8ba917677a7044c82c54db17d1e4781da82
joshuakelley <joshkelley19@gmail.com>
2017-02-18 14:46:48 -0500

    access material options

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,18 +9,25 @@ import { DataService } from '../shared/data.service';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  category: any;
+  // category: any;
   material: any;
+  categoryIndex: number;
   materialIndex: number;
+  result: any;
 
   constructor(public navCtrl: NavController, private dataService: DataService) {
-    this.category = '';
     this.material = '';
     this.materialIndex = -1;
+    this.categoryIndex = -1;
   }
 
   print(){
     console.log(this.dataService.data);
   }
 
+  getResult(result){
+    this.result = this.dataService.data[this.categoryIndex].materials[this.materialIndex];
+    console.log('Regularization Id: '+this.result.regularization);
+    console.log('Process Id: '+this.result.process);
+  }
 }
